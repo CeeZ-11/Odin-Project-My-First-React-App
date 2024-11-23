bfbb7a0a908e93f69d4eb6dbc0030379d9947006
Seamor Estrabon <seamorestrabon@gmail.com>
2024-11-24 00:37:56 +0800

    Add React components

---
+++ src/Food.jsx
@@ -0,0 +1,13 @@
+export function Food() {
+  return (
+    <>
+      <h1>My Favourite Food</h1>
+      <ul>
+        <li>Pizza</li>
+        <li>Burgers</li>
+        <li>Pasta</li>
+        <li>Sushi</li>
+      </ul>
+    </>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Greeting from "./Greeting.jsx";
+import { Food } from "./Food.jsx";
+import { App } from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Greeting />
+    <Food />
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
